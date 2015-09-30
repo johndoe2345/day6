8ef6f0050630a56b7761918fb2a4c4f92cbe1048
johndoe <johndoe2345646@vfemail.net>
2015-09-30 11:19:25 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/*var iceCream = ["pistacio pistacio", "phish food", "chubby hubby"];
+var candy = ["sour patch kids", "skittles", "candy corn"];
+var soda = ["coca cola", "root beer", "jarritos"];
+var food = [iceCream, candy, soda];
+console.log(food[2][2]);*/
+
